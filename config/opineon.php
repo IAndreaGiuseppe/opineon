f0595268c23e15271dab6eb14848418642c45818
Mindexperiment <andrewloyd@libero.it>
2019-03-15 20:13:25 +0100

    improved configuration

--- config/opineon.php
+++ config/opineon.php
@@ -5,6 +5,7 @@ return [
  /**
   * Define the basic models used by Opineon
   */
+ 'userModel'    => \App\User::class,
  'opinionModel' => \Agpretto\Opineon\Opinion::class,
  'commentModel' => \Agpretto\Opineon\Comment::class,
 
