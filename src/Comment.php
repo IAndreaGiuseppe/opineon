2b7127eede93283e0c88a1704acac7fcf45e6599
Mindexperiment <andrewloyd@libero.it>
2019-04-22 23:17:07 +0200

    fix interface that don't exists

--- src/Comment.php
+++ src/Comment.php
@@ -5,7 +5,7 @@ namespace Agpretto\Opineon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Comment extends Model implements Commentable {
+class Comment extends Model {
 
  use SoftDeletes;
 
